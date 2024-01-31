de7ba85747a21ea3f885840f9e219db94d64a473
Lonten Technology <142191915+LontenTechnology@users.noreply.github.com>
2024-01-31 09:19:08 +0800

    Add files via upload

---
+++ Arduino Code/Lesson 10 IR Receiver Module/IR_Receiver_Module/IR_Receiver_Module.ino
@@ -0,0 +1,30 @@
+#include "IRremote.h"
+
+int receiver = 11; // Signal Pin of IR receiver to Arduino Digital Pin 11
+
+/*-----( Declare objects )-----*/
+IRrecv irrecv(receiver);     // create instance of 'irrecv'
+decode_results results;      // create instance of 'decode_results'
+
+/*-----( Function )-----*/
+void translateIR() // takes action based on IR code received
+
+// describing Remote IR codes 
+{
+  Serial.println(results.value);
+  delay(500); // Do not get immediate repeat
+} //END translateIR
+void setup()   /*----( SETUP: RUNS ONCE )----*/
+{
+  Serial.begin(9600);
+  Serial.println("IR Receiver Button Decode"); 
+  irrecv.enableIRIn(); // Start the receiver
+}/*--(end setup )---*/
+void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+  if (irrecv.decode(&results)) // have we received an IR signal?
+  {
+    translateIR(); 
+    irrecv.resume(); // receive the next value
+  }  
+}/* --(end main loop )-- */
---
+++ Arduino Code/Lesson 23 DS1302 Clock Sensor/DS1302_Clock_Sensor/DS1302_Clock_Sensor.ino
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <DS1302.h>
+
+/* Set the appropriate digital I/O pin connections */
+uint8_t CE_PIN   = 5;
+uint8_t IO_PIN   = 6;
+uint8_t SCLK_PIN = 7;
+
+/* Create buffers */
+char buf[50];
+char day[10];
+
+/* Create a DS1302 object */
+DS1302 rtc(CE_PIN, IO_PIN, SCLK_PIN);
+
+void print_time()
+{
+  /* Get the current time and date from the chip */
+  Time t = rtc.time();
+
+  /* Name the day of the week */
+  memset(day, 0, sizeof(day));  /* clear day buffer */
+    
+  switch (t.day) {
+    case 1:
+      strcpy(day, "Sunday");
+      break;
+    case 2:
+      strcpy(day, "Monday");
+      break;
+    case 3:
+      strcpy(day, "Tuesday");
+      break;
+    case 4:
+      strcpy(day, "Wednesday");
+      break;
+    case 5:
+      strcpy(day, "Thursday");
+      break;
+    case 6:
+      strcpy(day, "Friday");
+      break;
+    case 7:
+      strcpy(day, "Saturday");
+      break;
+  }
+  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
+           day,
+           t.yr, t.mon, t.date,
+           t.hr, t.min, t.sec);
+  Serial.println(buf);
+}
+void setup()
+{  Serial.begin(9600);
+  rtc.write_protect(false);
+  rtc.halt(false);
+  Time t(2020, 8, 5, 11,12,13,4);
+  rtc.time(t);}
+void loop()
+{ print_time();
+  delay(1000);}
+
